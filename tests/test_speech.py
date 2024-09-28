a118c8ddd0c74a0c8e882695faaddb8c507088a5
Jesse <jesse-lee.hughes@outlook.com>
2024-09-29 08:59:02 +1000

    refactor and optimize

--- tests/test_speech.py
+++ tests/test_speech.py
@@ -1,24 +1,20 @@
 import os
 
-from library.audio.audio import convert_to_pcm_wav
 from library.speech import SpeechSynthesizer, SpeechTranscriber, UnsupportedFileType
 
 
-def test_convert_to_pcm_wav():
-    audio_file_path = os.path.expanduser('synthesized_audio.wav')
-    convert_to_pcm_wav(audio_file_path)
 
 
 def test_transcriber():
     speech_key = os.environ['SPEECH_API_KEY']
     service_region = os.environ['SPEECH_REGION']
-    audio_file_path = os.path.expanduser('audio.wav')
+    audio_file_path = os.path.expanduser('audio_files/audio.wav')
     input_language = 'en-US'
     output_language = 'fr'
     if not os.path.isfile(audio_file_path):
         raise FileNotFoundError(f"The audio file does not exist at {audio_file_path}")
 
-    transcibe_api = SpeechTranscriber(
+    transcribe_api = SpeechTranscriber(
         speech_key,
         service_region,
         audio_file_path,
@@ -29,16 +25,16 @@ def test_transcriber():
     if not speech_api.is_pcm_wav(audio_file_path):
         raise UnsupportedFileType("File type is not supported.")
 
-    translated_text = transcibe_api.transcribe()
+    translated_text = transcribe_api.transcribe()
     print(f"Translated Text: {translated_text}")
 
     # Synthesize the translated text into speech using 'yue' for Cantonese
     speech_api.synthesize_speech(
         translated_text,
         target_language=output_language,
-        output_path='~/repos/canto/uploads/synthesized_audio.wav'
-    )
+        output_path='audio_files/synthesized_audio.wav'
 
+    )
 
 if __name__ == '__main__':
     test_transcriber()
