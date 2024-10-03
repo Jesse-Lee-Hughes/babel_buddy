ca6aacc83deeefc0305a171bc1968c9d0589d7c4
Jesse <jesse-lee.hughes@outlook.com>
2024-10-04 07:10:00 +1000

    refactor and optimize

--- tests/test_speech.py
+++ tests/test_speech.py
@@ -1,40 +1,49 @@
 import os
+import unittest
 
 from library.speech import SpeechSynthesizer, SpeechTranscriber, UnsupportedFileType
 
 
+class TestSpeech(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        # Initialize class-level variables
+        cls.speech_key = os.environ.get('SPEECH_API_KEY')
+        cls.service_region = os.environ.get('SPEECH_REGION')
+        cls.input_language = 'en-US'
+        cls.output_language = 'yue'
+        cls.audio_file_path = os.path.expanduser('audio_files/audio.wav')
 
-def test_transcriber():
-    speech_key = os.environ['SPEECH_API_KEY']
-    service_region = os.environ['SPEECH_REGION']
-    audio_file_path = os.path.expanduser('audio_files/audio.wav')
-    input_language = 'en-US'
-    output_language = 'fr'
-    if not os.path.isfile(audio_file_path):
-        raise FileNotFoundError(f"The audio file does not exist at {audio_file_path}")
+        # Initialize APIs
+        cls.speech_api = SpeechSynthesizer(cls.speech_key, cls.service_region)
+        cls.transcribe_api = SpeechTranscriber(
+            cls.speech_key,
+            cls.service_region,
+            cls.audio_file_path,
+            input_language=cls.input_language,
+            output_language=cls.output_language,
+        )
 
-    transcribe_api = SpeechTranscriber(
-        speech_key,
-        service_region,
-        audio_file_path,
-        input_language=input_language,
-        output_language=output_language,
-    )
-    speech_api = SpeechSynthesizer(speech_key, service_region)
-    if not speech_api.is_pcm_wav(audio_file_path):
-        raise UnsupportedFileType("File type is not supported.")
+    def test_get_voices(self):
+        r = self.speech_api.speech_synthesizer.get_voices_async().get()
+        for voice in r.voices:
+            print(voice.name)
 
-    translated_text = transcribe_api.transcribe()
-    print(f"Translated Text: {translated_text}")
+    def test_transcriber(self):
 
-    # Synthesize the translated text into speech using 'yue' for Cantonese
-    speech_api.synthesize_speech(
-        translated_text,
-        target_language=output_language,
-        output_path='audio_files/synthesized_audio.wav'
+        if not os.path.isfile(self.audio_file_path):
+            raise FileNotFoundError(f"The audio file does not exist at {self.audio_file_path}")
 
-    )
+        if not self.speech_api.is_pcm_wav(self.audio_file_path):
+            raise UnsupportedFileType("File type is not supported.")
 
-if __name__ == '__main__':
-    test_transcriber()
+        translated_text = self.transcribe_api.transcribe()
+        self.transcribe_api.logger.debug(f"Translated Text: {translated_text}")
+
+        # Synthesize the translated text into speech using 'yue' for Cantonese
+        self.speech_api.synthesize_speech(
+            translated_text,
+            target_language=self.output_language,
+            output_path='audio_files/synthesized_audio.wav'
+        )
